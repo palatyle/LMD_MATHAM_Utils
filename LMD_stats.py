87d4b7aca839a58c427da659f9d6a2fa0b5b3da3
Tyler Paladino <palatyle@isu.edu>
2022-11-01 08:55:32 -0600

    Halfway w/powerset iterations

--- LMD_stats.py
+++ LMD_stats.py
@@ -12,7 +12,8 @@ def powerset(iterable):
     s = list(iterable)
     return chain.from_iterable(combinations(s,r) for r in range(len(s)+1))
 
-os.chdir('D:\\')
+    
+os.chdir('/Volumes/MATHAM4/')
 
 # Read in volcano location data
 df_volc = pd.read_csv('Mars_Volc_locs_no_Arsia.csv')
@@ -20,14 +21,14 @@ df_volc = pd.read_csv('Mars_Volc_locs_no_Arsia.csv')
 p_set = list(powerset(df_volc['Volcano Name']))
 
 # Read in GRS data
-df_GRS = pd.read_csv('C:\\Users\\palatyle\\Documents\\ArcGIS\\Projects\\Mars_GIS\\data\\GRS_data_raw.csv')
+df_GRS = pd.read_csv('/Users/tylerpaladino/Documents/ISU/Thesis/Mars_regolith_hydration/GRS_data/GRS_data_raw_180.csv')
 
 # Sort data and drop Center Lon column
-df_GRS_sorted = df_GRS.sort_values(by=['Lon_fix','CenterLat'],ascending=False)
+df_GRS_sorted = df_GRS.sort_values(by=['lon_180','CenterLat'],ascending=False)
 df_GRS_sorted = df_GRS_sorted.drop(columns=['CenterLon'])
 
 # Convert to numpy array and only grab unique vals
-GRS_lons = df_GRS_sorted['Lon_fix'].to_numpy()
+GRS_lons = df_GRS_sorted['lon_180'].to_numpy()
 GRS_lons = np.unique(GRS_lons)
 
 GRS_lats = df_GRS_sorted['CenterLat'].to_numpy()
@@ -39,28 +40,54 @@ GRS_lats = GRS_lats[::-1]
 GRS_lon,GRS_lat = np.meshgrid(GRS_lons,GRS_lats)
 
 # Interpolate (really just reshape) GRS data to meshgrid
-GRS_vals_grd = griddata((df_GRS['Lon_fix'].to_numpy(),df_GRS['CenterLat'].to_numpy()),df_GRS['Concentration'].to_numpy(),(GRS_lon,GRS_lat),method='nearest')
-plt.pcolormesh(GRS_lon,GRS_lat,GRS_vals_grd)
-plt.show
+GRS_vals_grd = griddata((df_GRS['lon_180'].to_numpy(),df_GRS['CenterLat'].to_numpy()),df_GRS['Concentration'].to_numpy(),(GRS_lon,GRS_lat),method='nearest')
 
+def interpolate_data(X,Y,Xi,Yi,arr):
+    # Set up interpolator object which has original grid data
+    interpolator = RegularGridInterpolator((X,Y),arr,method="nearest")
+    # Interpolate to Xi,Yi grid
+    return interpolator((Xi,Yi))
+
+def get_r_val(arr,arr_nans):
+    # Reshape to flat array
+    arr_flat = arr.reshape(arr.shape[0]*arr.shape[1])
+    arr_nans_flat = arr_nans.reshape(arr_nans.shape[0]*arr_nans.shape[1])
+    # Remove nans from both arrays
+    arr_flat_nn = arr_flat[~np.isnan(arr_nans_flat)]
+    arr_nans_flat_nn = arr_nans_flat[~np.isnan(arr_nans_flat)]
+    
+    return scipy.stats.pearsonr(arr_nans_flat_nn,arr_flat_nn)
+    
+    
 # Read in one LMD GCM output file ot grab lat/lon geo and create a meshgrid
 ds_volc = xr.open_dataset('all_volc_dep_norm.nc',mask_and_scale=False,decode_times=False)
 lat = ds_volc.latitude
 lon = ds_volc.longitude
 LMD_lon,LMD_lat = np.meshgrid(lon,lat)
 
-# Interpolate volc sum array to GRS grid. Plot to check everything is working. 
-interp = RegularGridInterpolator((lat,lon),ds_volc.volc_1_surf,method="nearest")
-volc_interp = interp((GRS_lat,GRS_lon))
-plt.pcolormesh(GRS_lon,GRS_lat,volc_interp)
-# plt.show()
 
-GRS_flat = GRS_vals_grd.reshape(GRS_vals_grd.shape[0]*GRS_vals_grd.shape[1])
-volc_interp_flat = volc_interp.reshape(volc_interp.shape[0]*volc_interp.shape[1])
 
-volc_interp_flat_nn = volc_interp_flat[~np.isnan(GRS_flat)]
-GRS_flat_nn = GRS_flat[~np.isnan(GRS_flat)]
+os.chdir('Indv_volcs')    
+dep_dict = {}
+for volc_name in df_volc['Volcano Name']:
+    temp = xr.open_dataset('diagfi_volc_filt_nccomp_' + volc_name + '.nc',mask_and_scale=False,decode_times=False)
+    
+    dep_dict[volc_name] = 
+volc_interp = interpolate_data(lat,lon,GRS_lat,GRS_lon,ds_volc.volc_1_surf)
+r = get_r_val(volc_interp,GRS_vals_grd)
 
-r = scipy.stats.pearsonr(GRS_flat_nn, volc_interp_flat_nn)
-print(r)
+# # Interpolate volc sum array to GRS grid. Plot to check everything is working. 
+# interp = RegularGridInterpolator((lat,lon),ds_volc.volc_1_surf,method="nearest")
+# volc_interp = interp((GRS_lat,GRS_lon))
+# plt.pcolormesh(GRS_lon,GRS_lat,volc_interp)
+# # plt.show()
+
+# GRS_flat = GRS_vals_grd.reshape(GRS_vals_grd.shape[0]*GRS_vals_grd.shape[1])
+# volc_interp_flat = volc_interp.reshape(volc_interp.shape[0]*volc_interp.shape[1])
+
+# volc_interp_flat_nn = volc_interp_flat[~np.isnan(GRS_flat)]
+# GRS_flat_nn = GRS_flat[~np.isnan(GRS_flat)]
+
+# r = scipy.stats.pearsonr(GRS_flat_nn, volc_interp_flat_nn)
+# print(r)
 print('stop')
